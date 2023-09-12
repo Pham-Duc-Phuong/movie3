4fb41747f3561093660e7e7568e0becbe9007135
Pham Duc Phuong <phuong.mom33@gmail.com>
2023-09-13 01:11:47 +0700

    s

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -54,13 +54,10 @@ const Detail = () => {
               </tbody>
             </table>
             <div className='mb-24'>
-              <button className="inline-flex items-center p-8 pl-14 pr-14 mr-28 text-20 font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-                // onClick={() => {
-                //   navigate('/list')
-                // }}
+              <a href={detailMovie.trailer} target='_blank'><button className="inline-flex items-center p-8 pl-14 pr-14 mr-28 text-20 font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
               >
                 Trailer
-              </button>
+              </button></a>
               <button className="inline-flex items-center p-8 pl-17 pr-17 text-20 font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
                 onClick={() => {
                   const path = generatePath(PATH.booking, { bookingid: detailMovie.maPhim })
