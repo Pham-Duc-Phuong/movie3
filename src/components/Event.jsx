dcbb9c6ec6121e645022303286d8ef6d4dc47ae4
Pham Duc Phuong <phuong.mom33@gmail.com>
2023-09-12 22:25:56 +0700

    s

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Event = () => {
   return (
-    <div className='text-white text-2xl text-center h-screen'>Event</div>
+    <div className='h-screen'>
+      <div className='w-bookingX h-bookingY bg-white translate-x-bookingX translate-y-bookingY rounded-lg flex justify-center items-center'>
+        <h1 className='text-8xl font-700 text-center leading-normal'>Hiện tại <br></br> chưa có sự kiện</h1>
+      </div>
+    </div>
   )
 }
 
--- src/components/Position.jsx
+++ src/components/Position.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Position = () => {
   return (
-    <div className='text-white text-2xl text-center h-screen'>Position</div>
+    <div className='h-screen'>
+      <div className='w-bookingX h-bookingY bg-white translate-x-bookingX translate-y-bookingY rounded-lg flex flex-col justify-center items-center'>
+        <h1 className='text-8xl font-700 text-center leading-normal '>Hệ thống rạp đang bảo trì</h1>
+        <p className='text-4xl font-500 text-blue-700 text-center leading-normal my-50'>Khách hàng có thể xem phim trực tuyến tại <a className='underline text-red-500' href="https://vlxx.moe/"> phimmoi.net</a></p>
+      </div>
+    </div>
   )
 }
 
